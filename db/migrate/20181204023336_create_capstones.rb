bbdb779facb9253842624853914fe663230eee48
“Joe <joseph.sherman@Loop.colum.edu>
2018-12-03 20:39:57 -0600

    Add Capstone, Education, Experience, Skill, and Student models.

---
+++ db/migrate/20181204023336_create_capstones.rb
@@ -0,0 +1,12 @@
+class CreateCapstones < ActiveRecord::Migration[5.2]
+  def change
+    create_table :capstones do |t|
+      t.integer :student_id
+      t.string :name
+      t.text :description
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
