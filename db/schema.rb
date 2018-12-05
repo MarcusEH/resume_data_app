42f650034f4af4fac797938fd4708ceb776372a2
Thomas Ng <D3icidal@gmail.com>
2018-12-04 20:24:51 -0500

    add add student controler and fix seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_05_002440) do
+ActiveRecord::Schema.define(version: 2018_12_05_003614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,56 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 10.times do 
-  lotr = Faker::LordOfTheRings.character
+  lotr = Faker::Movies::LordOfTheRings.character
   Student.create([ 
-    {first_name: lotr}, 
-    {last_name: Faker::Name.first_name}, 
-    {email: Faker::Internet.email},
-    {phone_number: Faker::PhoneNumber.cell_phone},
-    {short_bio: Faker::Overwatch.quote},
-    {linkedin_url: 'www.linked.com/users/'+lotr},
-    {twitter_handle: '@'+Faker::lotr},
-    {personal_blog_url: 'www.medium.com/'+lotr},
-    {online_resume_url: 'www.resume.com/' + lotr},
-    {github_url: 'www.github.com/u/' + lotr},
-    {photo_url: 'www.imgur.com/sample.jpg'}
+    {first_name: lotr, 
+    last_name: Faker::Name.first_name, 
+    email: Faker::Internet.email,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    short_bio: Faker::Overwatch.quote,
+    linkedin_url: 'www.linked.com/users/'+lotr,
+    twitter_handle: '@'+lotr,
+    personal_blog_url: 'www.medium.com/'+lotr,
+    online_resume_url: 'www.resume.com/' + lotr,
+    github_url: 'www.github.com/u/' + lotr,
+    photo_url: 'www.imgur.com/sample.jpg'}
 ])
+end
 
 20.times do
   Experience.create ([
-  {student_id: rand(1..10)},
-  {start_date: Time.now}, #TODO
-  {end_date: Time.now},
-  {job_title: Faker::Job.title},
-  {company_name: Faker::Company.name},
-  {details: Faker::Company.catch_phrase},
+  {student_id: rand(1..10),
+  start_date: Time.now, #TODO
+  end_date: Time.now,
+  job_title: Faker::Job.title,
+  company_name: Faker::Company.name,
+  details: Faker::Company.catch_phrase}
 ])
+end
 
 20.times do
   Education.create ([
-  {student_id: rand(1..10)},
-  {start_date: Time.now},
-  {end_date: Time.now},
-  {degree: Faker::Job.title},
-  {university_name: Faker::Company.name},
-  {details: Faker::Company.catch_phrase}
+  {student_id: rand(1..10),
+  start_date: Time.now,
+  end_date: Time.now,
+  degree: Faker::Job.title,
+  university_name: Faker::Company.name,
+  details: Faker::Company.catch_phrase}
 ])
+end
 
 20.times do
   Skill.create ([
-    {student_id: rand(1..10)},
-    {skill_name: 'ruby'}
+    {student_id: rand(1..10),
+    skill_name: 'ruby'}
   ])
+end
 
 20.times do
   Capstone.create ([
-    {student_id: rand(1..10)},
-    {name: Faker::Company.name},
-    {description: Faker::Company.catch_phrase},
-    {url: 'www.'+Faker::Company.name+'.com'}
+    {student_id: rand(1..10),
+    name: Faker::Company.name,
+    description: Faker::Company.catch_phrase,
+    url: 'www.'+Faker::Company.name+'.com'}
   ])
+end
