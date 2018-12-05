b05669c31f43460ed9530dd18c9fbb1a33a55b02
“Joe <joseph.sherman@Loop.colum.edu>
2018-12-04 18:40:40 -0600

    Add User model and users controller.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_023356) do
+ActiveRecord::Schema.define(version: 2018_12_05_002440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,14 @@ ActiveRecord::Schema.define(version: 2018_12_04_023356) do
     t.string "online_resume_url"
     t.string "github_url"
     t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "student_id"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
